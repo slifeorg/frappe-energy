bdc600c91ad26f6ae2429035f110a27c633568ba
Andrew Pavlichuk <andrewpavlicuk2@gmail.com>
2025-06-03 12:52:06 +0000

    Added "Go Back" button for page without sidebar

--- frappe/public/js/sidebar-toggle.js
+++ frappe/public/js/sidebar-toggle.js
@@ -2,13 +2,6 @@ frappe.router.on("change", function () {
     // Hiding the sidebar
     setTimeout(() => $(".list-sidebar, .form-sidebar").removeClass("show-sidebar"), 200);
 
-    // Adding a button for opening the sidebar
-    if ($(".custom-toggle-sidebar").length === 0) {
-        const toggleBtn = $(`<button class="custom-toggle-sidebar"><div class="burger-icon"></div></button>`);
-        toggleBtn.on('click', () => $(".list-sidebar, .form-sidebar").toggleClass("show-sidebar"));
-        $(".navbar").prepend(toggleBtn);
-    }
-
     // Waiting for the page to fully load
     const waitForActivePage = setInterval(() => {
         const $activePage = $(".page-container").filter(function () {
@@ -29,6 +22,15 @@ frappe.router.on("change", function () {
 function waitForSidebar($activePage, attempt = 0) {
     const sidebar = $activePage.find(".list-sidebar, .form-sidebar");
     if (sidebar.length > 0) {
+        $(".custom-go-back").remove();
+
+        // Adding a button for opening the sidebar
+        if ($(".custom-toggle-sidebar").length === 0) {
+            const toggleBtn = $(`<button class="custom-toggle-sidebar"><div class="burger-icon"></div></button>`);
+            toggleBtn.on('click', () => $(".list-sidebar, .form-sidebar").toggleClass("show-sidebar"));
+            $(".navbar").prepend(toggleBtn);
+        }
+
         // Checking, if sidebar-top already exists
         if (sidebar.find(".sidebar-top").length === 0) {
             const backBtn = $(`<button class="custom-close-sidebar"><div class="arrow-back-icon"></div></button>`);
@@ -47,7 +49,17 @@ function waitForSidebar($activePage, attempt = 0) {
     } else if (attempt < 20) {
         // Retrying every 100ms for 2s
         setTimeout(() => waitForSidebar($activePage, attempt + 1), 100);
+
+        if (attempt === 10) {
+            addBackButton($activePage);
+        }
     } else {
         console.warn("Sidebar not found on this page after waiting.");
     }
+}
+
+function addBackButton($activePage) {
+    const toggleBtn = $(`<button class="custom-go-back"><div class="arrow-back-icon"></div></button>`);
+    toggleBtn.on('click', () => window.history.back());
+    $(".navbar").prepend(toggleBtn);   
 }
